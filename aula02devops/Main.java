5bb664900d315badb72da2c78db62825f4dc70af
maikezzk <maikezzk7@gmail.com>
2026-08-13 22:04:08 -0300

    turquia

--- aula02devops/Main.java
+++ aula02devops/Main.java
@@ -3,7 +3,7 @@
 public class Main{
     //Crie função principal da classe Main
 
-    plublic static void main (String[]args){
+    public static void main (String[]args){
         System.out.println("Olá, Devops !");
         System.out.println("Pipeline executada com sucesso !");
     }
