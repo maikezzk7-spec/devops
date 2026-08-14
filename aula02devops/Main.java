ce68d7b4889dc73a4759fd4cc27369ce7bd36c3e
maikezzk <maikezzk7@gmail.com>
2026-08-13 21:51:09 -0300

    Turquia

---
+++ aula02devops/Main.java
@@ -0,0 +1,10 @@
+// Crie uma classe publica chamada Main
+
+public class Main{
+    //Crie função principal da classe Main
+
+    plublic static void main (String[]args){
+        System.out.println("Olá, Devops !");
+        System.out.println("Pipeline executada com sucesso !");
+    }
+}
